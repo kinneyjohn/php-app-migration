db5718e96cadcbe06580dd104cc0be37193b8000
John Kinney <kinney.john@gmail.com>
2024-07-29 20:04:07 -0400

    updating for gitops deployment

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,2 +1,5 @@
-# Deploy php webapp
-oc apply -k ../cluster/applications/webapp
+# Deploy php webapp using Kustomize
+#oc apply -k ../cluster/applications/webapp
+
+# Deploy through GitOps
+oc apply -f ../cluster/applications/webapp.yaml
